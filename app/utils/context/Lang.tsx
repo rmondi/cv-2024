1d523c1c2eb18e90e1543e18ab9b420587746b53
Rémy Mondi <remy@mondi.dev>
2024-12-04 10:06:13 +0100

    [Update] Store lang in localstorage

--- app/utils/context/Lang.tsx
+++ app/utils/context/Lang.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, createContext } from "react";
+import { useState, useEffect, createContext } from "react";
 
 /**
  * Context : lang
@@ -26,11 +26,25 @@ type LangProviderType = {
 
 export const LangProvider = ( { children }: LangProviderType ) => {
 
+  const [ isLoading, setIsloading ] = useState( true );
   const [ lang, setLang ] = useState( "fr" );
 
-  const updateLang = ( lang: string ) => setLang( lang );
+  const updateLang = ( lang: string ) => {
 
-  return (
+    localStorage.setItem( process.env.NEXT_PUBLIC_LOCALSTORAGE_NAME as string, lang );
+
+    setLang( lang );
+  };
+
+  useEffect( () => {
+    const storedLang = localStorage.getItem( process.env.NEXT_PUBLIC_LOCALSTORAGE_NAME as string );
+
+    if ( storedLang ) setLang( storedLang );
+
+    setIsloading( false );
+  }, [] );
+
+  if ( !isLoading ) return (
     <LangContext.Provider value={ { lang, updateLang } }>
       { children }
     </LangContext.Provider>
